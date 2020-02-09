72b732de4789822240a8627ca3b1ad88e751e948
David Borzek <thejoker77@t-online.de>
2020-02-09 11:03:56 +0100

    refactor: use rofi_status to decide if the dialog was closed

--- rofi-vscode.sh
+++ rofi-vscode.sh
@@ -8,18 +8,19 @@ newWorkspace="Create new workspace"
 
 chosen=$(echo "$(ls  $pathToWorkspaces)\n\n$newWorkspace" | rofi -dmenu -p "VSCode Workspace Selector > ")
 
+rofi_status=$?
+
 ### New Entry ###
 newWorkspaceName=""
+
+if [ ${rofi_status} = "1" ]; then
+		exit
+fi;
 
 if [ "$chosen" = "$newWorkspace" ]
 then
 	newWorkspaceName=$(rofi -dmenu -p "Workspace Name: ")
 	mkdir "$pathToWorkspaces$newWorkspaceName/" && code "$pathToWorkspaces$newWorkspaceName/"
 else
-	if [ "$chosen" = "" ]
-	then
-		exit 0
-	else
-		code "$pathToWorkspaces/$chosen/"
-	fi
+	code "$pathToWorkspaces/$chosen/"
 fi
