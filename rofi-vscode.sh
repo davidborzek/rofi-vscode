f4b04c7963b026ce2d929f9958553d4e051c27aa
David Borzek <thejoker77@t-online.de>
2020-02-09 11:20:28 +0100

    feat: add keybinding Alt+r to remove a selected workspace

--- rofi-vscode.sh
+++ rofi-vscode.sh
@@ -6,7 +6,7 @@ pathToWorkspaces=$(echo "$1")
 ### Options ###
 newWorkspace="Create new workspace"
 
-chosen=$(echo "$(ls  $pathToWorkspaces)\n\n$newWorkspace" | rofi -dmenu -p "VSCode Workspace Selector > ")
+chosen=$(echo "$(ls  $pathToWorkspaces)\n\n$newWorkspace" | rofi -dmenu -kb-custom-1 "Alt+r"  -p "VSCode Workspace Selector > ")
 
 rofi_status=$?
 
@@ -17,10 +17,26 @@ if [ ${rofi_status} = "1" ]; then
 		exit
 fi;
 
-if [ "$chosen" = "$newWorkspace" ]
-then
-	newWorkspaceName=$(rofi -dmenu -p "Workspace Name: ")
-	mkdir "$pathToWorkspaces$newWorkspaceName/" && code "$pathToWorkspaces$newWorkspaceName/"
-else
-	code "$pathToWorkspaces/$chosen/"
-fi
+if [ ${rofi_status} = "10" ]; then
+	shouldDelete=$(echo "Yes\nNo" | rofi -dmenu -p "Do you want to delete the workspace '$chosen'?");
+	echo $?
+	if [ ${?} = "1" ]; then
+		exit 
+	fi;
+
+	if [ ${?} = "0" ]; then
+		if [ ${shouldDelete} = "Yes" ]; then
+			rm -r "$pathToWorkspaces$chosen/"
+		fi;
+	fi;
+fi;
+
+if [ ${rofi_status} = "0" ]; then
+	if [ "$chosen" = "$newWorkspace" ];
+	then
+		newWorkspaceName=$(rofi -dmenu -p "Workspace Name: ")
+		mkdir "$pathToWorkspaces$newWorkspaceName/" && code "$pathToWorkspaces$newWorkspaceName/"
+	else
+		code "$pathToWorkspaces/$chosen/"
+	fi;
+fi;
