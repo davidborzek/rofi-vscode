13c5a81d4fce36486c6496a222a4e009191f27ac
David Borzek <thejoker77@t-online.de>
2020-02-09 14:04:29 +0100

    feat: new keybinding Alt+Return for workspace information with several options

--- rofi-vscode.sh
+++ rofi-vscode.sh
@@ -25,6 +25,7 @@ newWorkspace="Create new workspace"
 args=( -dmenu
 	-kb-custom-1 'Alt+r'
 	-kb-custom-2 'Alt+e'
+	-kb-custom-3 'Alt+Return'
 	-p 'VSCode Workspace Selector > '
 )
 
@@ -37,7 +38,7 @@ if [ ${rofi_status} = "1" ]; then
 fi;
 
 if [ ${rofi_status} = "10" ]; then
-	shouldDelete=$(echo "Yes\nNo" | rofi -dmenu -p "Do you want to delete the workspace '$chosen'?");
+	shouldDelete=$(echo -e "Yes\nNo" | rofi -dmenu -p "Do you want to delete the workspace '$chosen'?");
 	if [ ${?} = "1" ]; then
 		exit
 	fi;
@@ -52,11 +53,51 @@ fi;
 if [ ${rofi_status} = "11" ]; then
 	newName=$(rofi -dmenu -p "(ESC to abort) Rename the workspace '$chosen': ");
 	if [ ${?} = "1" ]; then
-		exit 
+		exit
 	fi;
 
 	if [ ${?} = "0" ]; then
 		mv "$pathToWorkspaces/$chosen/" "$pathToWorkspaces/$newName/"
+	fi;
+fi;
+
+if [ ${rofi_status} = "12" ]; then
+	operation=$(echo -e "Name: $chosen\nPath: $pathToWorkspaces/$chosen\n\nOpen\nEdit\nDelete" | rofi -dmenu -p "(ESC to abort) Workspace information: ");
+	if [ ${?} = "1" ]; then
+		exit 
+	fi;
+
+	if [ ${?} = "0" ]; then
+		echo "Yo"
+		if [ ${operation} = "Open" ]; then
+			code "$pathToWorkspaces/$chosen/"
+			exit
+		fi;
+
+		if [ ${operation} = "Edit" ]; then
+			newName=$(rofi -dmenu -p "(ESC to abort) Rename the workspace '$chosen': ");
+			if [ ${?} = "1" ]; then
+				exit 
+			fi;
+
+			if [ ${?} = "0" ]; then
+				mv "$pathToWorkspaces/$chosen/" "$pathToWorkspaces/$newName/"
+			fi;
+		fi;
+
+		if [ ${operation} = "Delete" ]; then
+			shouldDelete=$(echo -e "Yes\nNo" | rofi -dmenu -p "Do you want to delete the workspace '$chosen'?");
+			if [ ${?} = "1" ]; then
+				exit
+			fi;
+
+			if [ ${?} = "0" ]; then
+				if [ ${shouldDelete} = "Yes" ]; then
+					rm -r "$pathToWorkspaces/$chosen/"
+				fi;
+			fi;
+		fi;
+
 	fi;
 fi;
 
